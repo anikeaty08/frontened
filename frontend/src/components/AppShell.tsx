c7d97d5f6a5f6c3187daa6f905b64d106ecae587
anikeaty08 <aniketbxr1@gmail.com>
2026-08-12 14:46:56 +0530

    Add cursor twinkles across product routes

--- frontend/src/components/AppShell.tsx
+++ frontend/src/components/AppShell.tsx
@@ -1,9 +1,11 @@
 import SiteHeader from "./SiteHeader";
 import SiteFooter from "./SiteFooter";
+import GlobalCursorTwinkles from "./GlobalCursorTwinkles";
 
 export default function AppShell({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen">
+      <GlobalCursorTwinkles />
       <SiteHeader />
       <main>{children}</main>
       <SiteFooter />
